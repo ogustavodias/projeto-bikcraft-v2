66424b52409b30e89ea3d0c54de188061ed1bd9a
Gustavo Dias <gustavoalvesdiaz08@gmail.com>
2023-04-04 21:05:14 -0300

    add js code to check itens after load page

--- js/orcamento.js
+++ js/orcamento.js
@@ -1,8 +1,16 @@
+const parametros = Array.from(new URLSearchParams(location.search));
 /* context for menu  */
 {
     const menu1 = Array.from(document.querySelectorAll('.menu-1 label'));
     let menuToShow;
     let menuToHide;
+
+    document.body.onload = function checkMenuItensAfterLoad() {
+        const itemToCheck = document.querySelector(`.menu-1 input#${parametros[0][1]}`);
+        itemToCheck.checked = true;
+        menuToShow = document.querySelector(`div[data-menu="${itemToCheck.getAttribute('data-menu')}"]`);
+        menuToShow.classList.add('ativo');
+    }
 
     function showSubMenu(event) {
         menuToHide = menuToShow;
@@ -19,13 +27,27 @@
     }
 
     menu1.forEach(listening);
+
+
 }
 
-/* context for bike-items  */
+/* context for items  */
 {
     const menu2 = Array.from(document.querySelectorAll('.menu-2 label'));
     let detailToShow;
     let detailToHide;
+
+    /*     document.body.onload = function checkSubItensAfterLoad() { */
+    const itemToCheck = document.querySelector(`input#${parametros[1][1]}`);
+    itemToCheck.checked = true;
+
+    if (parametros[0][1] === 'bikcraft') {
+        detailToShow = document.querySelector(`div[data-bike="${parametros[1][1]}"`);
+        console.log(detailToShow);
+        detailToShow.classList.add('ativo');
+    }
+
+    /*     } */
 
     function showDetails(event) {
         detailToHide = detailToShow;
